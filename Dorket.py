24ba5301197f54d5cdb6efba26459552c9692fd5
Bojama <86303898+BojamaV@users.noreply.github.com>
2025-02-25 13:38:53 -0500

    Update Dorket.py

--- Dorket.py
+++ Dorket.py
@@ -1,36 +1,4 @@
 from googlesearch import search
 import time
-import colorama
-from colorama import Fore
 
-print("""
-   _,  
-  / |                  __        
- / ___,  ____ ___     /' |
- /'   `\,--,/'   `\  /'   |
-(       )  (       )'
- \_   _/'  `\_   _/         
-                                          
-      ~Dorket~
-    """)
-
-time.sleep(1)
-
-print(Fore.GREEN + 'Made by BojamaV')
-
-time.sleep(2)
-
-dork = input(Fore.RED + 'Enter your google dork: ')
-keyword = input(Fore.RED + 'Enter URL keywords: ')
-glob = input(Fore.RED + 'enter the link domain (com/gov/net): ')
-
-time.sleep(0.9)
-
-print(Fore.BLUE + 'Processing request...')
-
-time.sleep(1)
-
-search = f"intext:{dork} inurl:{keyword} site:*{glob}"
-search(search, sleep_interval=0.3, num_results=10000)
-
-print(search)
+search("Hey world")
