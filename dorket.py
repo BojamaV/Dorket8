27c6d5a29bcd7c2e09b3abbc8ba81a71d81e75c2
Bojama <86303898+BojamaV@users.noreply.github.com>
2025-03-18 19:13:09 -0400

    Update dorket.py

--- dorket.py
+++ dorket.py
@@ -1,7 +1,7 @@
 from googlesearch import search
 import time
-import colorama
 from colorama import Fore
+
 print("""
    _,  
   / |                  __        
@@ -12,26 +12,29 @@ print("""
                                           
       ~Dorket~
 """)
+
 time.sleep(1)
 print(Fore.GREEN + 'Made by BojamaV')
 time.sleep(1.5)
+
 print(Fore.GREEN + "!If you have no response just leave a * symbol or blank!")
 inurl = input('URL keyword: ')
 intext = input('Text in webpage: ')
 filetype = input('Filetype: ')
 topLevelDomain = input('TLD: ')
-related = input('related webpage: ')
+related = input('Related webpage: ')
+
 if filetype == '*':
     filetype = "doc | pdf | xls | txt | ps | rtf | odt | sxw | psw | ppt | pps | xml"
-x = ('inurl:' + inurl + ' intext:' + intext + ' filetype:' + filetype + ' site:*.' + topLevelDomain + ' related:' + related)
 
+search_query = f'inurl:{inurl} intext:{intext} filetype:{filetype} site:*.{topLevelDomain} related:{related}'
 
 time.sleep(0.9)
 print(Fore.BLUE + 'Processing request...')
 time.sleep(1)
-search_query=(x)
+
 try:
-    for i in search(search_query, lang='en', num=100, stop=150, pause=2.0):
+    for i in search(search_query, lang='en', stop=50, pause=2.0):
         print(i)
 except Exception as e:
     print(Fore.RED + f"Error: {e}")
